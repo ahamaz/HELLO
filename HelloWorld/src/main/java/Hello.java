58e300a0864a57616c83faf5920d04f87ac90c0c
ahasa <ahasa@DESKTOP-A8O5G04.station>
2021-10-25 18:26:10 +0100

    c5

--- HelloWorld/src/main/java/Hello.java
+++ HelloWorld/src/main/java/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi2");
-		System.out.println("hi4");
 		System.out.println("Finally done with grace of allah");
 	}
 
